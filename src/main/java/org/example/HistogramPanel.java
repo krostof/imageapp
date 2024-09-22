9f086b2df3cc2ca1bbadf20ca4fe087558792610
krostof <krostof4@wp.pl>
2024-09-22 18:06:41 +0200

    drawHistogram changes

--- src/main/java/org/example/HistogramPanel.java
+++ src/main/java/org/example/HistogramPanel.java
@@ -40,43 +40,33 @@ public class HistogramPanel extends JPanel {
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Find the maximum frequency for scaling
-        int maxFrequency = 0;
+        // Zsumowanie wartości RGB do odcieni szarości
+        int[] grayHistogram = new int[numBins];
         for (int i = 0; i < numBins; i++) {
-            for (int c = 0; c < 3; c++) {
-                if (histogram[c][i] > maxFrequency) {
-                    maxFrequency = histogram[c][i];
-                }
+            grayHistogram[i] = histogram[0][i] + histogram[1][i] + histogram[2][i];
+        }
+
+        // Znajdź maksymalną częstotliwość dla skalowania
+        int maxFrequency = 0;
+        for (int value : grayHistogram) {
+            if (value > maxFrequency) {
+                maxFrequency = value;
             }
         }
 
-        // Draw histogram bars
+        // Rysowanie histogramu w jednolitym kolorze
+        g2d.setColor(Color.BLUE); // Możesz zmienić kolor na czarny lub inny
         for (int i = 0; i < numBins; i++) {
-            int redHeight = (int) ((double) histogram[0][i] / maxFrequency * height);
-            int greenHeight = (int) ((double) histogram[1][i] / maxFrequency * height);
-            int blueHeight = (int) ((double) histogram[2][i] / maxFrequency * height);
-
-            // Draw bars for each channel
-            g2d.setColor(Color.RED);
-            g2d.fillRect(i * binWidth, height - redHeight, binWidth, redHeight);
-
-            g2d.setColor(Color.GREEN);
-            g2d.fillRect(i * binWidth + binWidth / 3, height - greenHeight, binWidth / 3, greenHeight);
-
-            g2d.setColor(Color.BLUE);
-            g2d.fillRect(i * binWidth + 2 * binWidth / 3, height - blueHeight, binWidth / 3, blueHeight);
+            int barHeight = (int) ((double) grayHistogram[i] / maxFrequency * height);
+            g2d.fillRect(i * binWidth, height - barHeight, binWidth, barHeight);
         }
 
-        // Draw axis labels
+        // Rysowanie etykiet osi
         g2d.setColor(Color.BLACK);
         g2d.drawString("0", 5, height - 5);
         g2d.drawString("255", width - 40, height - 5);
 
-        // Draw Y-axis labels
-        for (int y = 0; y <= height; y += 10) {
-            g2d.drawString(String.valueOf((height - y) * maxFrequency / height), 5, y);
-        }
-
         g2d.dispose();
     }
+
 }
